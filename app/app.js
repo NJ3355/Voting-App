c0b98f0c92fbfbe5f3565d9976e2051375d5dcb7
NJ3355 <njohn33555@gmail.com>
2016-08-30 14:16:55 -0400

    Adding a second controller

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+//pollbranch
+
 var votingApp = angular.module('votingApp', ['ngRoute', 'ngAnimate']);
 
 votingApp.config(['$routeProvider', function($routeProvider){
@@ -9,7 +11,7 @@ votingApp.config(['$routeProvider', function($routeProvider){
 		})
 		.when('/poll', {
 			templateUrl: 'views/poll.html',
-			controller: 'VotingController'
+			controller: 'PollController'
 		})
 		/*.when('/contact-success', {
 			templateUrl: 'views/contact-success.html',
@@ -87,6 +89,75 @@ votingApp.controller('VotingController', ['$scope', '$location', '$route', funct
 		 $route.reload();
 
 	};
+
+
+	$scope.vote = function(answer){
+		answer.score++;
+		answer.progress++
+		$scope.poll.totalVotes++;
+
+		$scope.calcPercent();
+		
+
+	};
+
+	$scope.calcPercent = function(){
+		
+
+		for(var i = 0; i < $scope.poll.answers.length; i++){
+			$scope.poll.answers[i].percent = Math.round(($scope.poll.answers[i].score/$scope.poll.totalVotes) * 100);
+		}
+
+	};
+
+
+
+
+}]);
+
+
+
+
+
+
+
+votingApp.factory('Data', function(){
+
+	return {
+			data: {
+			question: '',
+			totalVotes: 0,
+			answers: []
+		};
+	}
+
+});
+
+
+
+votingApp.controller('PollController', ['$scope', '$location', '$route', function($scope, $location, $route){
+		
+		$scope.pollAnswers = document.getElementById('answers');
+		
+		$scope.poll = {
+			question: '',
+			totalVotes: 0,
+			answers: []
+		};
+
+		$scope.choices = [];
+	//Available shifts main functions
+
+
+
+
+
+	$scope.redirect = function(path){
+console.log(path);
+		 $location.path(path);
+
+	};
+
 
 
 	$scope.vote = function(answer){
